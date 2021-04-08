627712b7bd7d03042ebdcdaf880ab63b79163f50
Johannes Völker <j.voelker@fodjan.de>
2021-04-08 20:00:46 +0200

    add some more checks prepare new resource-types

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,34 @@
 echo $CA | base64 -d > ca.crt
 
 # check parameters
+if [[ -z "${RESOURCE}" ]]; then
+  NAMESPACE="deployment"
+fi
 if [[ -z "${NAMESPACE}" ]]; then
   NAMESPACE="default"
 fi
 if [[ -z "${URL}" ]]; then
   echo "URL not set"
+  exit 1
+fi
+if [[ -z "${CONTEXT}" ]]; then
+  echo "CONTEXT not set"
+  exit 1
+fi
+if [[ -z "${TOKEN}" ]]; then
+  echo "TOKEN not set"
+  exit 1
+fi
+if [[ -z "${DEPLOYMENT}" ]]; then
+  echo "DEPLOYMENT not set"
+  exit 1
+fi
+if [[ -z "${CONTAINER}" ]]; then
+  echo "CONTAINER not set"
+  exit 1
+fi
+if [[ -z "${IMAGE}" ]]; then
+  echo "IMAGE not set"
   exit 1
 fi
 
@@ -18,18 +41,19 @@ rancher login $URL --token $TOKEN --context $CONTEXT
 # Patch deployment
 echo ""
 echo ""
-echo "########### UPDATE CLUSER #################"
+echo "########### UPDATE CLUSTER #################"
+echo "Resource.............. $RESOURCE"
 echo "Namespace............. $NAMESPACE"
 echo "Deployment............ $DEPLOYMENT"
 echo "Container............. $CONTAINER"
 echo "Image................. $IMAGE"
-echo "########### UPDATE CLUSER #################"
-rancher kubectl --namespace $NAMESPACE patch deployment $DEPLOYMENT --type strategic --patch  '{"spec": {"template": {"spec": {"containers": [{"name": "'$CONTAINER'","image": "'$IMAGE'"}]}}}}'
+echo "########### UPDATE CLUSTER #################"
+rancher kubectl --namespace $NAMESPACE patch $RESOURCE $DEPLOYMENT --type strategic --patch  '{"spec": {"template": {"spec": {"containers": [{"name": "'$CONTAINER'","image": "'$IMAGE'"}]}}}}'
 echo "patch done, wait for rollout..."
 
 # Check deployment rollout status every 10 seconds (max 10 minutes) until complete.
 attempts=0
-rollout_status_cmd="rancher kubectl rollout status deployment $DEPLOYMENT --namespace $NAMESPACE "
+rollout_status_cmd="rancher kubectl rollout status $RESOURCE $DEPLOYMENT --namespace $NAMESPACE "
 until $rollout_status_cmd; do
   $rollout_status_cmd
   attempts=$((attempts + 1))
